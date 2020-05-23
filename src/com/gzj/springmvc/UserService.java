4b4536078c26649aec3460f92af379fab7837abb
yuel_global <13373657935_global@163.com>
2020-05-23 09:45:50 +0800

    1

---
+++ src/com/gzj/springmvc/UserService.java
@@ -0,0 +1,15 @@
+package com.gzj.springmvc;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+//	@Autowired
+//	private HelloWorld helloWorld;
+	
+	public UserService() {
+		System.out.println("UserService Constructor...");
+	}
+	
+}
